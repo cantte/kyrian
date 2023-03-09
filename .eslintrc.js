db850e79baff59e449d25b1f18f822b534e3fe64
Carlos Pavajeau <cantte098@gmail.com>
2023-03-09 15:55:22 -0500

    chore: format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ["kyrian"], // uses the config in `packages/config/eslint`
-  parser: "@typescript-eslint/parser",
+  extends: ['kyrian'], // uses the config in `packages/config/eslint`
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
     tsconfigRootDir: __dirname,
     project: [
-      "./tsconfig.json",
-      "./apps/*/tsconfig.json",
-      "./packages/*/tsconfig.json",
+      './tsconfig.json',
+      './apps/*/tsconfig.json',
+      './packages/*/tsconfig.json',
     ],
   },
   settings: {
     next: {
-      rootDir: ["apps/kyrian"],
+      rootDir: ['apps/kyrian'],
     },
   },
-};
+}
 
-module.exports = config;
+module.exports = config
