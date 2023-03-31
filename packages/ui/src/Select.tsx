f97184c3a45d8ed1e4b3d3b93f154fa13ef600f9
Carlos Pavajeau <cantte098@gmail.com>
2023-03-31 10:02:00 -0500

    feat(ui): add select component

---
+++ packages/ui/src/Select.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import {
+  forwardRef,
+  type ComponentPropsWithoutRef,
+  type ElementRef,
+} from 'react'
+import * as SelectPrimitive from '@radix-ui/react-select'
+import { Check, ChevronDown } from 'lucide-react'
+
+import { cn } from './lib/utils'
+
+const Select = SelectPrimitive.Root
+
+const SelectGroup = SelectPrimitive.Group
+
+const SelectValue = SelectPrimitive.Value
+
+const SelectTrigger = forwardRef<
+  ElementRef<typeof SelectPrimitive.Trigger>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+>(({ className, ...props }, forwardedRef) => (
+  <SelectPrimitive.Trigger
+    ref={forwardedRef}
+    className={cn(
+      'flex h-10 w-full items-center justify-between rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
+      className,
+    )}
+    {...props}
+  >
+    {props.children}
+    <ChevronDown className='h-4 w-4 opacity-50' />
+  </SelectPrimitive.Trigger>
+))
+
+SelectTrigger.displayName = '@kyrian/ui/SelectTrigger'
+
+const SelectContent = forwardRef<
+  ElementRef<typeof SelectPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({ className, children, ...props }, forwardedRef) => (
+  <SelectPrimitive.Portal>
+    <SelectPrimitive.Content
+      ref={forwardedRef}
+      className={cn(
+        'animate-in fade-in-80 relative z-50 min-w-[8rem] overflow-hidden rounded-md border border-slate-100 bg-white text-slate-700 shadow-md dark:border-slate-800 dark:bg-slate-800 dark:text-slate-400',
+        className,
+      )}
+      {...props}
+    >
+      <SelectPrimitive.Viewport className='p-1'>
+        {children}
+      </SelectPrimitive.Viewport>
+    </SelectPrimitive.Content>
+  </SelectPrimitive.Portal>
+))
+
+SelectContent.displayName = '@kyrian/ui/SelectContent'
+
+const SelectLabel = forwardRef<
+  ElementRef<typeof SelectPrimitive.Label>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Label>
+>(({ className, ...props }, forwardedRef) => (
+  <SelectPrimitive.Label
+    ref={forwardedRef}
+    className={cn(
+      'py-1.5 pr-2 pl-8 text-sm font-semibold text-slate-900 dark:text-slate-300',
+      className,
+    )}
+    {...props}
+  />
+))
+
+SelectLabel.displayName = '@kyrian/ui/SelectLabel'
+
+const SelectItem = forwardRef<
+  ElementRef<typeof SelectPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, forwardedRef) => (
+  <SelectPrimitive.Item
+    ref={forwardedRef}
+    className={cn(
+      'relative flex cursor-default select-none items-center rounded-sm py-1.5 pr-2 pl-8 text-sm font-medium outline-none focus:bg-slate-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-slate-700',
+      className,
+    )}
+    {...props}
+  >
+    <span className='absolute left-2 flex h-3.5 w-3.5 items-center justify-center'>
+      <SelectPrimitive.ItemIndicator>
+        <Check className='h-4 w-4' />
+      </SelectPrimitive.ItemIndicator>
+    </span>
+
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+))
+
+SelectItem.displayName = '@kyrian/ui/SelectItem'
+
+const SelectSeparator = forwardRef<
+  ElementRef<typeof SelectPrimitive.Separator>,
+  ComponentPropsWithoutRef<typeof SelectPrimitive.Separator>
+>(({ className, ...props }, forwardedRef) => (
+  <SelectPrimitive.Separator
+    ref={forwardedRef}
+    className={cn('-mx-1 my-1 h-px bg-slate-100 dark:bg-slate-700', className)}
+    {...props}
+  />
+))
+
+SelectSeparator.displayName = '@kyrian/ui/SelectSeparator'
+
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+}
