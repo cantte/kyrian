6809b36019ddc891bf52a352f7baaeef58151ce8
Carlos Pavajeau <cantte098@gmail.com>
2023-03-31 10:02:19 -0500

    feat(ui): export added components

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,10 +1,46 @@
-import { Button } from '@kyrian/ui'
+import {
+  Button,
+  Input,
+  Label,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@kyrian/ui'
 
 export default function Web() {
   return (
     <div className='m-10 px-10'>
       <h1>Web</h1>
-      <Button>Default</Button>
+
+      <div className='my-2'></div>
+
+      <div className='grid grid-cols-1 gap-4'>
+        <div>
+          <Button>Default</Button>
+        </div>
+
+        <div>
+          <div className='grid w-full max-w-sm items-center gap-1.5'>
+            <Label htmlFor='email'>Email</Label>
+            <Input type='email' id='email' placeholder='Email' />
+          </div>
+        </div>
+
+        <div>
+          <Select>
+            <SelectTrigger className='w-[180px]'>
+              <SelectValue placeholder='Theme' />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value='light'>Light</SelectItem>
+              <SelectItem value='dark'>Dark</SelectItem>
+              <SelectItem value='system'>System</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
     </div>
   )
 }
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,4 +1,7 @@
 import './styles.css'
 
 export { default as Button } from './Button'
+export { default as Input } from './Input'
+export { default as Label } from './Label'
+export * from './Select'
 export * from './lib/utils'
