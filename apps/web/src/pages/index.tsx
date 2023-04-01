0303506f137c3a34ffa640af9eb7c087b11ab5d0
Carlos Pavajeau <cantte098@gmail.com>
2023-04-01 12:52:05 -0500

    feat(ui): add checkbox component

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import {
   AvatarFallback,
   AvatarImage,
   Button,
+  Checkbox,
   Input,
   Label,
   Select,
@@ -49,6 +50,18 @@ export default function Web() {
             <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
+        </div>
+
+        <div>
+          <div className='flex items-center space-x-2'>
+            <Checkbox id='terms' />
+            <label
+              htmlFor='terms'
+              className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+            >
+              Accept terms and conditions
+            </label>
+          </div>
         </div>
       </div>
     </div>
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,6 +2,7 @@ import './styles.css'
 
 export * from './Avatar'
 export { default as Button } from './Button'
+export * from './Checkbox'
 export { default as Input } from './Input'
 export { default as Label } from './Label'
 export * from './Select'
