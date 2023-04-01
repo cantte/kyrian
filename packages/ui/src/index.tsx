9497057f263a421febcbf8ad079f9f707c2e05ba
Carlos Pavajeau <cantte098@gmail.com>
2023-04-01 13:23:50 -0500

    feat(ui): add dialog component

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import './styles.css'
 
 export * from './Avatar'
 export { default as Button } from './Button'
 export * from './Checkbox'
+export * from './Dialog'
 export { default as Input } from './Input'
 export { default as Label } from './Label'
 export * from './Select'
