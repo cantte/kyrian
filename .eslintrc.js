67894f16a452804411ba3ab3aa540eea4478f15d
Carlos Pavajeau <cantte098@gmail.com>
2023-04-04 18:58:16 -0500

    chore: update configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: ['kyrian'], // uses the config in `packages/config/eslint`
+  extends: ['eslint-config-kyrian'], // uses the config in `packages/config/eslint`
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
