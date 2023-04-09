a1e9635802fe7e1535bd55879f5058d0b68e2496
Carlos Pavajeau <cantte098@gmail.com>
2023-04-08 23:49:47 -0500

    build(ui): include use client directive

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig, type Options } from 'tsup'
 
 export default defineConfig((options: Options) => ({
-  treeshake: true,
+  treeshake: false,
   splitting: true,
-  entry: ['src/**/*.tsx'],
+  entry: ['src/**/*.tsx', 'src/**/*.ts'],
   format: ['esm'],
   dts: true,
   minify: true,
   clean: true,
+  // include 'use client' in the output
+  banner: {
+    js: "'use client';",
+  },
+  keepNames: true,
   external: ['react'],
   ...options,
 }))
