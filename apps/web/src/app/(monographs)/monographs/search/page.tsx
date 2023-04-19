0f439dd2b952b40e2f33db84221d2a84eb19af70
cantte <cantte098@gmail.com>
2023-04-19 12:44:56 -0500

    feat(monographs): improve search page ui
    * use card for show all monographs

--- apps/web/src/app/(monographs)/monographs/search/page.tsx
+++ apps/web/src/app/(monographs)/monographs/search/page.tsx
@@ -1,11 +1,20 @@
 import { redirect } from 'next/navigation'
 import { createServerSideHelpers } from '@trpc/react-query/server'
+import { Link } from 'lucide-react'
 import { getServerSession } from 'next-auth/next'
 import superjson from 'superjson'
 
 import { appRouter } from '@kyrian/api'
 import { authOptions } from '@kyrian/auth'
 import { prisma } from '@kyrian/db'
+import {
+  Button,
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@kyrian/ui'
 
 type SearchMonographsPageProps = {
   searchParams: { [key: string]: string | string[] | undefined }
@@ -35,9 +44,29 @@ const SearchMonographsPage = async ({
 
   return (
     <>
-      <ul>
+      <ul className='app-grid app-gap-3 app-w-full px-2 py-2'>
         {monographs.map((monograph) => (
-          <li key={monograph.id}>{monograph.title}</li>
+          <li key={monograph.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{monograph.title}</CardTitle>
+                <CardDescription>
+                  Publicado el{' '}
+                  {Intl.DateTimeFormat('es-CO', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                  }).format(monograph.publicationDate)}
+                </CardDescription>
+              </CardHeader>
+
+              <CardFooter className='app-flex app-justify-end md:app-justify-start'>
+                <Button>
+                  <Link className='app-mr-2 app-h-4 app-w-4' /> Ver
+                </Button>
+              </CardFooter>
+            </Card>
+          </li>
         ))}
       </ul>
     </>
