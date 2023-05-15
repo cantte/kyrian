17bbe48760e5e991134bbf5a02c15824b9774a93
cantte <cantte098@gmail.com>
2023-05-15 14:47:48 -0500

    fix: solve ui bugs

--- apps/web/src/app/(degree-programs)/degree-programs/new/page.tsx
+++ apps/web/src/app/(degree-programs)/degree-programs/new/page.tsx
@@ -16,7 +16,7 @@ const RegisterNewDegreeProgramPage = async () => {
     <div className='app-grid app-items-start app-gap-8 app-min-w-2xl'>
       <div className='app-flex app-justify-between'>
         <div className='app-grid app-gap-1'>
-          <h1 className='app-text-2xl app-font-bold app-tracking-wide app-text-slate-900'>
+          <h1 className='app-scroll-m-20 app-text-4xl app-font-extrabold app-tracking-tight lg:app-text-5xl'>
             Registro de programas de grado
           </h1>
         </div>
