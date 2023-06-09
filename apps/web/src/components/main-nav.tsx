6408595d1e9c4126dd94117728fd398339e96a96
cantte <cantte098@gmail.com>
2023-06-08 19:38:22 -0500

    feat: add cedisj logo

--- apps/web/src/components/main-nav.tsx
+++ apps/web/src/components/main-nav.tsx
@@ -6,6 +6,7 @@ import { useSelectedLayoutSegment } from 'next/navigation'
 
 import { cn } from '@kyrian/ui'
 
+import { Icons } from '~/components/icons'
 import { type MainNavItem } from '~/types'
 
 type Props = {
@@ -22,19 +23,22 @@ const MainNav = ({ items }: Props) => {
         href='/'
         className='app-hidden app-items-center app-space-x-2 md:app-flex'
       >
+        <Icons.cedisj className='app-w-12 app-h-12' />
         <span className='app-hidden app-font-bold sm:app-inline-block app-text-primary'>
           CEDISJ
         </span>
       </Link>
       {items?.length ? (
-        <nav className='app-hidden app-gap-6 md:app-flex app-text-sm app-font-medium'>
+        <nav className='app-hidden app-gap-6 md:app-flex'>
           {items?.map((item, index) => (
             <Link
               key={index}
               href={item.disabled ? '#' : item.href}
               className={cn(
-                'app-transition-colors app-text-foreground/60',
-                item.href.startsWith(`/${segment}`) && 'app-text-foreground',
+                'app-flex app-items-center app-text-lg app-font-medium app-transition-colors hover:app-text-foreground/80 sm:app-text-sm',
+                item.href.startsWith(`/${segment}`)
+                  ? 'app-text-foreground'
+                  : 'app-text-foreground/60',
                 item.disabled && 'app-cursor-not-allowed app-opacity-80',
               )}
             >
