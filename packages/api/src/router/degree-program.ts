85992ce7ab95e3620fcc666070bdff5f7605595c
Carlos Pavajeau <cantte098@gmail.com>
2023-07-02 19:04:55 -0500

    feat(degree-programs): add api procedures for edit

--- packages/api/src/router/degree-program.ts
+++ packages/api/src/router/degree-program.ts
@@ -1,6 +1,11 @@
 import * as z from 'zod'
 
-import { newDegreeProgramSchema } from '../schemas/degree-program'
+import {
+  editDegreeProgramSchema,
+  newDegreeProgramObjectiveSchema,
+  newDegreeProgramProfileSchema,
+  newDegreeProgramSchema,
+} from '../schemas/degree-program'
 import { createTRPCRouter, protectedProcedure, publicProcedure } from '../trpc'
 
 export const degreeProgramRouter = createTRPCRouter({
@@ -40,6 +45,48 @@ export const degreeProgramRouter = createTRPCRouter({
         },
         where: {
           code: input.code,
+        },
+      })
+    }),
+  edit: protectedProcedure
+    .input(editDegreeProgramSchema)
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.degreeProgram.update({
+        data: input,
+        where: {
+          code: input.code,
+        },
+        include: {
+          objectives: true,
+          profiles: true,
+        },
+      })
+    }),
+  addProfile: protectedProcedure
+    .input(newDegreeProgramProfileSchema)
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.degreeProgramProfiles.create({
+        data: {
+          ...input,
+          degreeProgram: {
+            connect: {
+              code: input.degreeProgramCode,
+            },
+          },
+        },
+      })
+    }),
+  addObjective: protectedProcedure
+    .input(newDegreeProgramObjectiveSchema)
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.degreeProgramObjectives.create({
+        data: {
+          ...input,
+          degreeProgram: {
+            connect: {
+              code: input.degreeProgramCode,
+            },
+          },
         },
       })
     }),
