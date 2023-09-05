2d128a81576c25cf633bef3288472103b24eac45
cantte <cantte098@gmail.com>
2023-09-05 13:09:43 -0500

    build: don't transpile ui module

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  transpilePackages: ['@kyrian/ui', '@kyrian/auth', '@kyrian/db'],
+  transpilePackages: ['@kyrian/auth', '@kyrian/db'],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.node$/,
