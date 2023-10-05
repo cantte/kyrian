b2f082fd0bd93481010275ba32f2a13d2ae551eb
Carlos Pavajeau <cantte098@gmail.com>
2023-10-04 23:15:51 -0500

    feat: initial version of edit pages

---
+++ apps/web/src/app/(monographs)/monographs/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth/next'
+
+import { authOptions } from '@kyrian/auth'
+
+import PageUnderBuild from '~/components/feedback/page-under-build'
+
+const EditMonographPage = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    return redirect('/api/auth/signin')
+  }
+
+  if (session.user.role !== 'admin') {
+    return redirect('/')
+  }
+
+  return <PageUnderBuild />
+}
+
+export default EditMonographPage
